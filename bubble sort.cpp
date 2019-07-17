c935d56db2038fdb2bd5bb1942640f8e0528d974
mahfuz840 <43561160+mahfuz840@users.noreply.github.com>
2019-07-17 17:18:21 +0600

    Update bubble sort.cpp

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -8,7 +8,7 @@ int main()
 	for(int i = 0; i < n; i++)
 		scanf("%d", &a[i]);
 
-	for(int i = 0; i < n-1; i++)
+	for(int i = 0; i < n-i-1; i++)
 		for(int j = 0; j < n-1; j++)
 			if(a[j] > a[j+1])
 			{
